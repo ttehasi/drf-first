bbddd77aa243f70015588e58a5789846f2b40549
ttehasi <tatiana.chudowa@my.com>
2025-11-24 09:39:02 +0300

    optimize view

--- web/app/feedback/serializers.py
+++ web/app/feedback/serializers.py
@@ -6,9 +6,18 @@ from .models import DemoForm
 class DemoFormSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='name_of_requester')
     organisation_name = serializers.CharField(source='org_name')
-    organisation_type = serializers.IntegerField(write_only=True)
+    organisation_type = serializers.IntegerField(source='org_type', write_only=True)
     organization_type = serializers.CharField(source='org_type', read_only=True)
     objects = serializers.IntegerField(source='quantity_objects')
+    
+    def validate_organisation_type(self, value):
+        MAPP = {
+                1: 'Управляющая компания',
+                2: 'Застройщик',
+                3: 'Охранное предприятие',
+                4: 'Другое',   
+            }
+        return MAPP[value]
     
     class Meta:
         model = DemoForm
